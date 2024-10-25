0cbb323fb06d91848607ecee0e121e3f6e1c5aee
rra30 <163290584+rra30@users.noreply.github.com>
2024-10-25 23:01:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,5 +15,8 @@ st.set_page_config(
 st.title("Fire/smoke-detection Project :fire:")
 source_model = st.sidebar.radio("Select Source",["Model_Create","Model_test"])
 
+if source_model == "Model_test":
+    st.sidebar.radio("Choose Model type",["Test_Image","Test_Video"])
+
 if source_model == "Model_Create":
     st.sidebar.radio("Choose Model type",["Load_Model","study_Model"])
