4435759317dc1f43813a891589b13d066e872f96
rra30 <163290584+rra30@users.noreply.github.com>
2024-10-25 22:55:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ import numpy as np
 import io
 import PIL
 from PIL import Image
+
+st.set_page_config(
+    page_title="test",
+    page_icon="🌚",
+    layout="centered",
+    initial_sidebar_state="expanded")
