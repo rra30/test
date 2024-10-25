1971e954370d62f2f469ab52884ce8fd557c0f3f
rra30 <163290584+rra30@users.noreply.github.com>
2024-10-26 00:55:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,20 +20,7 @@ st.title("Test-Project-🌚")
 with st.sidebar:
     user_input = st.text_input("directory list",".")
     #os.listdir(user_input)
-    
-    def list_files(startpath):
-        tree_structure = ""
-        for root, dirs, files in os.walk(startpath):
-            level = root.replace(startpath, '').count(os.sep)
-            indent = ' ' * 4 * level
-            tree_structure += f"{indent}{os.path.basename(root)}/\n"
-            subindent = ' ' * 4 * (level + 1)
-            for f in files:
-                tree_structure += f"{subindent}{f}\n"
-        return tree_structure
-        
-    tree = list_files(user_input)
-    st.write(tree)
+    st.text("test")
     st.title(st.write("PATH : ",
                       os.getcwd()),
              st.write("subdirectory : ",os.listdir(path=".")))
