31941bd0096a8599ddd46536bf83644ae6b4de9f
rra30 <163290584+rra30@users.noreply.github.com>
2024-10-27 00:11:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,19 +17,20 @@ st.set_page_config(
 
 st.title("Test-Project-🌚")
 
+def list_files(startpath):
+    tree_structure = ""
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * level
+        tree_structure += f"{indent}{os.path.basename(root)}/\n"
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            tree_structure += f"{subindent}{f}\n"
+    return tree_structure
+
 with st.sidebar:
     user_input = st.text_input("directory list",".")
     #os.listdir(user_input)
-    def list_files(startpath):
-        tree_structure = ""
-        for root, dirs, files in os.walk(startpath):
-            level = root.replace(startpath, '').count(os.sep)
-            indent = ' ' * 4 * level
-            tree_structure += f"{indent}{os.path.basename(root)}/\n"
-            subindent = ' ' * 4 * (level + 1)
-            for f in files:
-                tree_structure += f"{subindent}{f}\n"
-        return tree_structure
     tree = list_files(user_input)
     st.text(tree)
     #st.text("test")
