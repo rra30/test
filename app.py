fb6a932d2b3eb02b42d3c2f5ee962e9daef55b13
rra30 <163290584+rra30@users.noreply.github.com>
2024-10-29 18:59:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ if source_model == "Model_test":
 #if source_model == "Model_Create":
     # 추가
 
+input = None
 if source_model == "Test_Image":
     st.sidebar.header("Imgae_Upload")
     input = st.sidebar.file_uploader("Choose an image.", type=("jpg","png"))
@@ -75,12 +76,6 @@ with st.sidebar:
             user_input = st.text_input("directory list",".")
             tree = list_files(user_input)
             st.text(tree)
-
-    if input is not None:
-        uploaded_image = PIL.Image.open(input)
-        uploaded_image_cv =cv2.cvtColor(np.array(uploaded_image), cv2.COLOR_RGB2BGR)
-        visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold = conf_threshold)
-        st.image(visualized_image, channels = "BGR")
 
 if source_model == "Test_Video":
     try:
