cff9f69e5c453ab1bbcd1270f41ef07bde11c7d1
rra30 <163290584+rra30@users.noreply.github.com>
2024-10-29 19:39:40 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ from ultralytics.utils.plotting import colors
 #model_path = './best_openvino_model/best.xml'
 
 #label_map = ['', '']
-try:
+if model_path != "./" or model path != "" and label_map != "":
     core = ov.Core()
 
     model = core.read_model(model=model_path)
