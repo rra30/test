b841cb7ea04efe43815d94fe5a3cbfc3b8eaf23e
rra30 <163290584+rra30@users.noreply.github.com>
2024-10-29 18:27:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ source_model = st.sidebar.radio("Select Model",["Model_Create","Model_test"])
 
 if source_model == "Model_test":
     with st.sidebar:
+        st.sidebar.file_uploader("Upload Model", type=(""))
         model_path = st.text_input("model path : ","./")
         st.write(model_path)
         label_map = st.text_input("model label : ","")
