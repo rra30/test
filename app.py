4fe90a131487beb12280a867fb99abda7274d8ee
rra30 <163290584+rra30@users.noreply.github.com>
2024-12-20 20:12:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+import streamlit as st
 import numpy as np
 import openvino as ov
 import cv2
+import io
+import PIL
+from PIL import Image
 import os
 import shutil
+#import utils
 
 st.set_page_config(
     page_title="Fire/smoke-detection",
