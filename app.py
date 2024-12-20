525243eda1a4d19634c1b5da4ce51870cd2a837f
rra30 <163290584+rra30@users.noreply.github.com>
2024-12-20 20:09:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,29 @@ import openvino as ov
 import cv2
 import os
 import shutil
+
+def play_video(video_source):
+    camera = cv2.VideoCapture(video_source)
+
+    st_frame = st.empty()
+    while(camera.isOpened()):
+        ret, frame = camera.read()
+
+        if ret:
+            visualized_image = utils.predict_image(frame, conf_threshold)
+            st_frame.image(visualized_image, channels = "BGR")
+        else:
+            camera.release()
+            break
+
+st.set_page_config(
+    page_title="Fire/smoke-detection",
+    page_icon="🔥",
+    layout="centered",
+    initial_sidebar_state="expanded")
+
+st.title("Fire/smoke-detection Project :fire:")
+source_radio = st.sidebar.radio("Select Source",["IMAGE","VIDEO","WEBCAM"])
+
+st.sidebar.header("Confidence")
+conf_threshold = float(st.sidebar.slider("Select the Confidence Threshold", 10, 100, 20))/100
