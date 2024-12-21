9696b39d2fafda353d7d4839e0e67291a79dc310
rra30 <163290584+rra30@users.noreply.github.com>
2024-12-21 12:16:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,14 @@ import os
 import utils
 import datetime as dt
 
+csv_directory = False
+
+directory = os.listdir()
+
+for i in range(len(directory)):
+    if directory.endswitch('.csv'):
+        csv_directory = True
+    
 csv_name = ""
 
 now = dt.datetime.now()
@@ -28,7 +36,37 @@ st.sidebar.header("Setting")
 for i in range(len(os.listdir())):
         
 
-if ():
+if (csv_directory == False):
     source_model = st.sidebar.radio("The data is not available.",["Data Collection"])
+    if source_model == "Data Collection"
 else:
     source_model = st.sidebar.radio("Select",["Data Collection","Utilize collected data"])
+    
+    if (source_model == "Utilize collected data"):
+        try:
+            if (csv_directory != True): #  학습된 데이터로 이동
+                single_data = []
+                Multiple_data = [['neutral', 'happy', 'sad', 'surprise', 'anger']]
+    
+    
+                columns = ['neutral', 'happy', 'sad', 'surprise', 'anger']
+            
+                for i in range(len(total_data)):
+                    for j in range(5):
+                        single_data.append(total_data[i][j])
+                    for a in range(5):
+                        Multiple_data[i].append(single_data[i])
+                    Multiple_data.append([])
+    
+                f = open(f"{csv_name}.csv", "w")
+                writer = csv.writer(f)
+
+                writer.writerows(Multiple_data)
+                f.close()
+        except:
+            pass
+    
+    elif (source_model == "Data Collection"):
+        a
+            
+
